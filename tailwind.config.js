3e41e12653522bb2f1633cbe8bbe8dc557ed6f53
shimizu <noon3m.op.sh@gmail.com>
2024-07-25 15:29:54 +0900

    [add]使いたい色の追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,17 @@ export default {
     extend: {
       colors: {
         primary: "#306CFE",
+        black: "#141414",
+        purple: "#48319D",
+        lightPurple: "#48319D",
+        "gradation-s": "#1C1B33",
+        "gradation-e": "#2E335A",
       },
       fontFamily: {
         sans: ["Potta One", "Yusei Magic"],
+      },
+      backgroundImage: {
+        "main-back": "url('/src/assets/ba')",
       },
     },
   },
