deaba475da5179a6499835f687871800600b4cc2
shimizu <noon3m.op.sh@gmail.com>
2024-07-26 02:39:23 +0900

    [change]メインからDB接続関数

--- back/cmd/main.go
+++ back/cmd/main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/noonyuu/benki/back/db"
+	"github.com/noonyuu/benki/back/router"
+)
+
+func loadEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Panicln("読み込み出来ませんでした: %v", err)
+	}
+}
 
 func main() {
+	loadEnv()
 	fmt.Println("Hello, World!")
-	database, err:= db.Connect(os.Getenv("MONGODB_URI"))
+	// データベース接続
+	database, err := db.Connect(os.Getenv("DB_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Disconnect()
+
+	router.Server(database)
+
+	// サーバーの終了を待機する
+	select {}
 }
