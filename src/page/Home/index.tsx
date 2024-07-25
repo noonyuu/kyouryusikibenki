8d38b98f51fab4a925a0e6a70503cfc2cb01f0a3
shimizu <noon3m.op.sh@gmail.com>
2024-07-25 15:32:02 +0900

    [add]メイン画面

--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,5 +1,19 @@
+import { BottomSheet } from "@/component/BottomSheet";
+import { Header } from "@/component/Header";
+import RandomText from "@/component/RandomText";
 import React from "react";
 
 export const Home: React.FC = () => {
-  return <div>Home: Rect.FC</div>;
+  const texts = ["Hello", "World", "Random", "Text", "React", "TypeScript"];
+  return (
+    <>
+      <Header />
+      <main className="relative h-[calc(100vh_-_64px_-_64px)] w-screen">
+        {texts.map((text, index) => (
+          <RandomText key={index} text={text} />
+        ))}
+      </main>
+      <BottomSheet />
+    </>
+  );
 };
