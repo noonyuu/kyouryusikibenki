25ad7c859d2aeb88416e154c08321f59702eb8ec
shimizu <noon3m.op.sh@gmail.com>
2024-07-27 15:39:07 +0900

    [change]navの修正

--- front/src/component/Header/index.tsx
+++ front/src/component/Header/index.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
-export const Header = () => {
+interface WordProps {
+  wordList: string[];
+}
+
+export const Header: React.FC<WordProps> = ({ wordList }) => {
   const [openMenu, setOpenMenu] = useState(false);
 
   const handleMenuOpen = () => {
     setOpenMenu(!openMenu);
+    console.log("wordList", wordList);
   };
 
   return (
@@ -19,8 +24,8 @@ export const Header = () => {
             <div
               className={
                 openMenu
-                  ? "bg-primary h-1 w-8 translate-y-2.5 rotate-45 transition duration-500 ease-in-out"
-                  : "bg-primary h-1 w-5 transition duration-500 ease-in-out"
+                  ? "h-1 w-8 translate-y-2.5 rotate-45 bg-primary transition duration-500 ease-in-out"
+                  : "h-1 w-5 bg-primary transition duration-500 ease-in-out"
               }
             />
             <div
@@ -33,18 +38,25 @@ export const Header = () => {
             <div
               className={
                 openMenu
-                  ? "bg-primary h-1 w-8 -rotate-45 transition duration-500 ease-in-out"
-                  : "bg-primary absolute right-0 h-1 w-5 transition duration-500 ease-in-out"
+                  ? "h-1 w-8 -rotate-45 bg-primary transition duration-500 ease-in-out"
+                  : "absolute right-0 h-1 w-5 bg-primary transition duration-500 ease-in-out"
               }
             />
           </button>
           <nav
             className={
-              openMenu
-                ? "absolute left-0 top-0 z-40 h-full w-full bg-black"
-                : "hidden"
+              openMenu ? "absolute left-0 top-0 z-40 h-full w-full" : "hidden"
             }
-          />
+          >
+            <div className="pt-4 bg-black text-center text-2xl text-white">
+              排泄待ち
+            </div>
+            <div className="pt-8 h-full overflow-y-auto bg-black text-center text-2xl text-white">
+              {wordList.map((word, index) => (
+                <div key={index}>{word}</div>
+              ))}
+            </div>
+          </nav>
         </span>
         {/* ハンバーガーメニューのマージンと横幅分左にずらす */}
         <span className="mt-3 flex grow -translate-x-11 items-center justify-center font-sans text-5xl text-black">
