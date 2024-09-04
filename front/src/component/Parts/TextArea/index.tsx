1438e0b2dcc222f724353100dca6a22863d30a49
shimizu <noon3m.op.sh@gmail.com>
2024-09-04 23:36:28 +0900

    [add]エイリアス設定したりファイル作ったり

---
+++ front/src/component/Parts/TextArea/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+
+type TextArea = {
+  onClick: () => void;
+  onChange: () => void;
+};
+
+export const TextArea: FC<TextArea> = () => {
+  return <div>index</div>;
+};
--- front/src/page/Form/index.tsx
+++ front/src/page/Form/index.tsx
@@ -1,15 +1,22 @@
 import { BottomSheet } from "@/component/BottomSheet";
 import { Header } from "@/component/Header";
+import { TextArea } from "@/component/Parts/TextArea";
 import { WordListHook } from "@/hook/WordList";
 
 export const Form = () => {
-  
   const wordList = WordListHook();
 
   return (
     <>
       <Header wordList={wordList} />
-
+      <TextArea
+        onClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+        onChange={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
       <BottomSheet />
     </>
   );
