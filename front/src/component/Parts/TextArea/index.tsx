0e8209fb9b9bb5f633704a3ea8271449eb4cdb03
shimizu <noon3m.op.sh@gmail.com>
2024-09-05 12:24:48 +0900

    [add]テキストエリアコンポーネント作成

--- front/src/component/Parts/TextArea/index.tsx
+++ front/src/component/Parts/TextArea/index.tsx
@@ -1,10 +1,23 @@
-import React, { FC } from "react";
+import { ChangeEvent, FC } from "react";
 
-type TextArea = {
-  onClick: () => void;
-  onChange: () => void;
+type TextAreaProps = {
+  description: string;
+  value: string;
+  onClickHandle: () => void;
+  onChangeHandle: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 };
 
-export const TextArea: FC<TextArea> = () => {
-  return <div>index</div>;
+export const TextArea: FC<TextAreaProps> = (props) => {
+  return (
+    <>
+      <textarea
+        wrap="hard"
+        className="w-4/5 rounded-bl-3xl rounded-tl-xl rounded-tr-3xl border border-black bg-white px-4 py-3 text-black"
+        placeholder={props.description}
+        value={props.value}
+        onChange={props.onChangeHandle}
+        onClick={props.onClickHandle}
+      />
+    </>
+  );
 };
