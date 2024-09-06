5ece9d3398888b7a962b6c2941fe42ce6a92d65e
shimizu <noon3m.op.sh@gmail.com>
2024-09-07 04:08:09 +0900

    [add]rhfとzod

--- front/src/page/Form/index.tsx
+++ front/src/page/Form/index.tsx
@@ -2,11 +2,52 @@ import { BottomSheet } from "@/component/BottomSheet";
 import { Header } from "@/component/Header";
 import { TextArea, Left, Right } from "@/component/Parts/TextArea";
 import { WordListHook } from "@/hook/WordList";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { getPrefecture } from "@/common/location";
+import { localStorage } from "@/common/localstrage";
+import { Input } from "@/component/Parts/Input/input";
+import { RfhSchema } from "@/schema/input/rfh.schema";
+import { getJapanesePrefectureName } from "@/common/prefecture";
+import { Prefecture } from "@/schema/input/zod.schema";
 
 export const Form = () => {
   const wordList = WordListHook();
+  const { register, errors, onSubmit, setValue } = RfhSchema();
   const [inputValue, setInputValue] = useState<string>("");
+  const [prefecture, setPrefecture] = useState<string | undefined>(undefined);
+
+  const isPrefecture = (value: string): value is Prefecture => {
+    return prefecture!.includes(value as Prefecture);
+  };
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem("prefecture");
+    if (userInfo) {
+      setValue("prefecture", userInfo as Prefecture);
+    }
+  }, [setValue]);
+
+  const onChangeHandle = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  useEffect(() => {
+    // 非同期関数を定義し、呼び出す
+    const fetchPrefecture = async () => {
+      try {
+        const prefecture = await getPrefecture();
+        if (prefecture) {
+          const japanesePrefecture = getJapanesePrefectureName(prefecture);
+          localStorage.setItem("prefecture", japanesePrefecture);
+          setValue("prefecture", japanesePrefecture as Prefecture);
+        }
+      } catch (error) {
+        console.error("Error fetching prefecture:", error);
+      }
+    };
+
+    fetchPrefecture();
+  }, [setValue]);
 
   // Left型のdirectionオブジェクト
   const leftDirection: Left = {
@@ -20,32 +61,84 @@ export const Form = () => {
     prepay: "rounded-bl-3xl",
   };
 
-  const onChangeHandle = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setInputValue(e.target.value);
+  const handleButtonClick = async () => {
+    try {
+      const prefecture = await getPrefecture();
+      if (!prefecture) {
+        return;
+      }
+      localStorage.setItem("prefecture", prefecture);
+    } catch (error) {
+      console.error("Error fetching prefecture:", error);
+    }
   };
 
   return (
     <>
       <Header wordList={wordList} />
-      <TextArea
-        onChangeHandle={onChangeHandle}
-        description={"にゅうりょくしてな"}
-        value={inputValue}
-        direction={leftDirection}
-      />
-      <TextArea
-        onChangeHandle={onChangeHandle}
-        description={"にゅうりょくしてな"}
-        value={inputValue}
-        direction={rightDirection}
-      />
-      <button
-        onClick={() => {
-          console.log(inputValue);
-        }}
-      >
-        ボタン
-      </button>
+      <main className="flex flex-col items-center space-y-4">
+        <div className="relative mt-4 flex max-w-[80%] items-center border p-4">
+          <div className="absolute -left-4 -top-4 border bg-white">任意</div>
+          <form onSubmit={onSubmit} className="space-y-2">
+            <p className="flex">
+              <div className="w-16 text-center">年齢</div>
+              <div className="w-32">
+                ：
+                <Input
+                  register={register("age", { required: true })}
+                  type="number"
+                />
+              </div>
+              <span className="text-red-200">{errors.age?.message}</span>
+            </p>
+            <p className="flex">
+              <div className="w-16 text-center">都道府県</div>
+              <div className="w-32">
+                ：
+                <input
+                  {...register("prefecture")}
+                  type="text"
+                  disabled
+                  className="w-24 rounded-md border bg-white"
+                />
+              </div>
+              <span className="text-xs text-red-200">
+                {errors.prefecture?.message}
+              </span>
+            </p>
+            <p className="flex">
+              <div className="w-16 text-center">性別</div>
+              <div>
+                ：
+                <select {...register("gender")}>
+                  <option value="">選択してください</option>
+                  <option value="男">男</option>
+                  <option value="女">女</option>
+                  <option value="その他">その他</option>
+                </select>
+                <span className="text-red-200">{errors.gender?.message}</span>
+              </div>
+            </p>
+            <button type="submit">ここ</button>
+          </form>
+        </div>
+        <div className="max-w-[80%] resize-none rounded-br-3xl rounded-tl-3xl rounded-tr-3xl border border-black bg-white px-4 py-3 text-black">
+          入力した情報は本サービスの利用規約に則って利用されます
+        </div>
+        <TextArea
+          onChangeHandle={onChangeHandle}
+          description={"にゅうりょくしてな"}
+          value={inputValue}
+          direction={rightDirection}
+        />
+        <button
+          onClick={() => {
+            handleButtonClick();
+          }}
+        >
+          ボタン
+        </button>
+      </main>
       <BottomSheet />
     </>
   );
---
+++ front/src/schema/input/zod.schema.ts
@@ -0,0 +1,83 @@
+import { z } from "zod";
+
+// 都道府県リスト
+const PREFECTURES = [
+  "北海道",
+  "青森県",
+  "岩手県",
+  "宮城県",
+  "秋田県",
+  "山形県",
+  "福島県",
+  "茨城県",
+  "栃木県",
+  "群馬県",
+  "埼玉県",
+  "千葉県",
+  "東京都",
+  "神奈川県",
+  "新潟県",
+  "富山県",
+  "石川県",
+  "福井県",
+  "山梨県",
+  "長野県",
+  "岐阜県",
+  "静岡県",
+  "愛知県",
+  "三重県",
+  "滋賀県",
+  "京都府",
+  "大阪府",
+  "兵庫県",
+  "奈良県",
+  "和歌山県",
+  "鳥取県",
+  "島根県",
+  "岡山県",
+  "広島県",
+  "山口県",
+  "徳島県",
+  "香川県",
+  "愛媛県",
+  "高知県",
+  "福岡県",
+  "佐賀県",
+  "長崎県",
+  "熊本県",
+  "大分県",
+  "宮崎県",
+  "鹿児島県",
+  "沖縄県",
+] as const;
+
+export type Prefecture = (typeof PREFECTURES)[number];
+
+// Zod スキーマ
+export const zodSchema = z.object({
+  age: z
+    .string()
+    .nullable()
+    .refine((val) => val === null || val === "" || /^[0-9]+$/.test(val), {
+      message: "年齢は数値である必要があります",
+    })
+    .refine(
+      (val) =>
+        val === null || val === "" || (val.length >= 1 && val.length <= 2),
+      { message: "不適切な値です" },
+    )
+    .transform((val) => (val ? Number(val) : null)),
+  prefecture: z
+    .enum(PREFECTURES, { message: "無効な都道府県です" })
+    .refine((value) => PREFECTURES.includes(value), {
+      message: "無効な都道府県です",
+    }),
+  gender: z
+    .string()
+    .nullable()
+    .refine((val) => val === "" || val === "男" || val === "女", {
+      message: "男か女かその他選択してください",
+    }),
+});
+
+export type ZodSchemaType = z.infer<typeof zodSchema>;
