04749d8e51f102aeb56beeb020ecea9e6016cc0e
shimizu <noon3m.op.sh@gmail.com>
2024-09-06 12:24:28 +0900

    [change]チャットバブルの吹き出し用にunionの定義

--- front/src/component/Parts/TextArea/index.tsx
+++ front/src/component/Parts/TextArea/index.tsx
@@ -1,20 +1,41 @@
 import { ChangeEvent, FC } from "react";
 
+// 左方向の定義
+export type Left = {
+  direction: "left";
+  prepay: string;
+};
+
+// 右方向の定義
+export type Right = {
+  direction: "right";
+  prepay: string;
+};
+
+// カスタムUnion型
+type Direction = Left | Right;
+
 type TextAreaProps = {
   description: string;
   value: string;
   onChangeHandle: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  direction: Direction;
 };
 
-export const TextArea: FC<TextAreaProps> = (props) => {
+export const TextArea: FC<TextAreaProps> = ({
+  description,
+  value,
+  onChangeHandle,
+  direction,
+}) => {
   return (
     <>
       <textarea
         wrap="hard"
-        className="w-4/5 resize-none rounded-bl-3xl rounded-tl-xl rounded-tr-3xl border border-black bg-white px-4 py-3 text-black"
-        placeholder={props.description}
-        value={props.value}
-        onChange={props.onChangeHandle}
+        className={`w-4/5 resize-none ${direction.prepay} rounded-tl-xl rounded-tr-3xl border border-black bg-white px-4 py-3 text-black`}
+        placeholder={description}
+        value={value}
+        onChange={onChangeHandle}
       />
     </>
   );
--- front/src/page/Form/index.tsx
+++ front/src/page/Form/index.tsx
@@ -1,12 +1,24 @@
 import { BottomSheet } from "@/component/BottomSheet";
 import { Header } from "@/component/Header";
-import { TextArea } from "@/component/Parts/TextArea";
+import { TextArea, Left, Right } from "@/component/Parts/TextArea";
 import { WordListHook } from "@/hook/WordList";
 import { ChangeEvent, useState } from "react";
 
 export const Form = () => {
   const wordList = WordListHook();
   const [inputValue, setInputValue] = useState<string>("");
+
+  // Left型のdirectionオブジェクト
+  const leftDirection: Left = {
+    direction: "left",
+    prepay: "rounded-br-3xl",
+  };
+
+  // Right型のdirectionオブジェクト
+  const rightDirection: Right = {
+    direction: "right",
+    prepay: "rounded-bl-3xl",
+  };
 
   const onChangeHandle = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setInputValue(e.target.value);
@@ -19,6 +31,13 @@ export const Form = () => {
         onChangeHandle={onChangeHandle}
         description={"にゅうりょくしてな"}
         value={inputValue}
+        direction={leftDirection}
+      />
+      <TextArea
+        onChangeHandle={onChangeHandle}
+        description={"にゅうりょくしてな"}
+        value={inputValue}
+        direction={rightDirection}
       />
       <button
         onClick={() => {
