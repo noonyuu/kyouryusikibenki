1cd7c78c0eb76ef7d2bffa4ad3b3e693daf3c0b9
shimizu <noon3m.op.sh@gmail.com>
2024-09-16 19:50:56 +0900

    [add]テキストエリアのバリデーションスキーマ

---
+++ front/src/page/Home/schema/FormSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const formSchema = z.object({
+  textInput: z
+    .string({
+      required_error: "何か叫んでね",
+      invalid_type_error: "それは叫べないよ",
+    })
+    .min(2, { message: "2文字以上入力してください" })
+    .max(50, { message: "50文字以内で入力してください" }),
+});
+
+export type formSchema = z.input<typeof formSchema>;
