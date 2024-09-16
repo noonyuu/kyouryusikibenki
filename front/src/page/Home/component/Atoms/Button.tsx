93bfadc493069a526c0439d9dad1083307345011
shimizu <noon3m.op.sh@gmail.com>
2024-09-16 16:59:29 +0900

    [add]ボタンコンポーネント

---
+++ front/src/page/Home/component/Atoms/Button.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+
+type ButtonProps = {
+  text: string;
+  bgColor: string;
+  textColor: string;
+  onSubmit: () => void;
+};
+
+export const Button: FC<ButtonProps> = ({
+  text,
+  bgColor,
+  textColor,
+  onSubmit,
+}) => {
+  return (
+    <button
+      className={`bg-${bgColor} text-${textColor} h-10 w-20 rounded-full px-4 py-2`}
+      onClick={onSubmit}
+    >
+      {text}
+    </button>
+  );
+};
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -10,6 +10,8 @@ export default {
         lightPurple: "#48319D",
         "gradation-s": "#1C1B33",
         "gradation-e": "#2E335A",
+
+        lightBlue: "#1D9BF0",
       },
       fontFamily: {
         sans: ["Potta One", "Yusei Magic"],
