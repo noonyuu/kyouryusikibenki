0d43122c0a6749a0eeefd621de30c7c336c96dfd
shimizu <noon3m.op.sh@gmail.com>
2024-09-18 02:44:00 +0900

    [add]ローディング画面を追加して受け取ったパスに合わせてurlを作成

--- front/src/page/Home/component/Templetes/PostList.tsx
+++ front/src/page/Home/component/Templetes/PostList.tsx
@@ -1,11 +1,18 @@
 import useSWR from "swr";
 import { PostCard } from "../Organism/PostCard";
+import { FC, useEffect } from "react";
+import styles from "../../turnAnimation.module.css";
+import { loopAnimation } from "@/common/reload";
 
 type Word = {
   ID: string;
   Word: string;
   Day: string;
   DeleteAt: string;
+};
+
+type PostListProps = {
+  path?: string;
 };
 
 const fetcher = (url: string) =>
@@ -16,17 +23,34 @@ const fetcher = (url: string) =>
     },
   }).then((res) => res.json());
 
-export const PostList = () => {
-  const { data, error, isLoading } = useSWR<Word[]>(
-    "https://benki.noonyuu.com/app/v1/word-list/all",
-    fetcher,
-  );
+const url = "https://benki.noonyuu.com/app/v1/word-list";
+
+export const PostList: FC<PostListProps> = ({ path }) => {
+  console.log(path);
+  const customPath = path ? url + path : url;
+
+  const { data, error, isLoading } = useSWR<Word[]>(customPath, fetcher);
+
+  useEffect(() => {
+    loopAnimation("logo", styles.target, 2000);
+  }, []);
 
   if (error) return <div>エラーが発生しました</div>;
-  if (isLoading) return <div>読み込み中...</div>;
+  if (isLoading)
+    return (
+      <div className="text-center text-white">
+        <img
+          id="logo"
+          className={`${styles.target} mx-auto mt-24 size-60`}
+          src={"unk.svg"}
+          alt="unko"
+        />
+        ...読み込み中
+      </div>
+    );
   console.log(data?.length);
   return (
-    <div>
+    <>
       {data?.map((item: Word, index: number) => (
         <PostCard
           key={index}
@@ -36,6 +60,6 @@ export const PostList = () => {
           deleteAt={""}
         />
       ))}
-    </div>
+    </>
   );
 };
