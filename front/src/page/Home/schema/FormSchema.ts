7c0f15ca0c5f23ebf271cfd6c31c98d88835e88b
shimizu <noon3m.op.sh@gmail.com>
2024-09-17 10:34:22 +0900

    [change]zodのスキーマを修正

--- front/src/page/Home/schema/FormSchema.ts
+++ front/src/page/Home/schema/FormSchema.ts
@@ -7,7 +7,9 @@ export const formSchema = z.object({
       invalid_type_error: "それは叫べないよ",
     })
     .min(2, { message: "2文字以上入力してください" })
-    .max(50, { message: "50文字以内で入力してください" }),
+    .max(50, { message: "50文字以内で入力してください" })
+    .trim()
+    .nonempty("何か叫んでね"),
 });
 
 export type formSchema = z.input<typeof formSchema>;
